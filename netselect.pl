08e72164305e52192861652966cb6999dbd6fe37
Eoetct Nahn <eotect@gmail.com>
2012-11-02 00:01:45 +0800

    Update hosts, IPTable and some scripts.

--- netselect.pl
+++ netselect.pl
@@ -1,27 +1,12 @@
 #!/usr/bin/perl -w
-use strict;
-use warnings;
-use lib '.';
-use IPTable;
-our $IPTable;
 
-sub netselect {
-	open FI,'-|','sudo','netselect','-vv',@_;
-	my $msg = join("",<FI>);
-	close FI;
-	if($msg =~ m/\s*\d+\s+([\d\.]+)/) {
-		return $1;
-	}
-	return "";
-}
+my @args = @ARGV;
 
-foreach(@ARGV) {
-	my $table = $IPTable->{$_};
-	if($table) {
-		print STDERR "Select fastest ip for $_:\n";
-		print join("\n",netselect(@{$table})),"\n";
-	}
-	else {
-		print STDERR "no ip found for $_ in \"google.pm\"\n";
+if(!@args) {
+	print STDERR "Reading ip from STDIN:\n";
+	while(<STDIN>) {
+		chomp;
+		push @args,$_;
 	}
 }
+system("sudo","netselect","-vv",@args);
